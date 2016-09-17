2e6568aa0efe91654b30ffa52c265b5e602146ca
Caio Saldanha <mrcaiosaldanha@gmail.com>
2016-09-16 21:57:10 -0300

    Adicionando umas besteirinhas.

--- src/AFD.java
+++ src/AFD.java
@@ -1,4 +1,13 @@
+import java.util.ArrayList;
 
 public class AFD {
-
+	
+	final int MAX_STATE = 100;
+	
+	private ArrayList<String>[][] auto;
+	
+	public AFD(){
+		auto = new ArrayList[MAX_STATE][MAX_STATE];
+	}
+	
 }
--- src/AFN.java
+++ src/AFN.java
@@ -1,4 +1,13 @@
+import java.util.ArrayList;
 
 public class AFN {
-
+	
+	final int MAX_STATE = 100;
+	
+	private ArrayList<String>[][] auto;
+	
+	public AFN(){
+		auto = new ArrayList[MAX_STATE][MAX_STATE];
+	}
+	
 }
