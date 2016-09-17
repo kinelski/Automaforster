c7ac0bc9d66981c795b4fc9650d623a5cf95f2f9
Gabriel Brito <gabriel.brito55@gmail.com>
2016-09-16 23:21:21 -0300

    Criadas funções booleanas

--- src/Auto.java
+++ src/Auto.java
@@ -8,16 +8,72 @@ public class Auto {
 	
 	private boolean isAFD = false;
 	private boolean noEps = false;
+	private int ini;
+	private int fim;
 	
 	public Auto(){
 		auto = new ArrayList[MAX_STATE][MAX_STATE];
+		for(int i = 0; i < 100; i++)
+			for(int j = 0; j < 100; j++){
+				auto[i][j] = new ArrayList<String>();
+			}
 	}
 	
 	private void addEdge (int s1, int s2, String w){
-		if (auto[s1][s2] == null)
-			auto[s1][s2] = new ArrayList<String>();
-		
 		auto[s1][s2].add(w);
+	}
+	
+	public void createNFAFromRE(String regex){
+		ini = 0;
+		fim = 1;
+		
+		auto[0][1].add(regex);
+		
+	}
+	
+	private void processaRegex(String substring){
+		if(isUnion(substring)){
+			//Altera a matriz para união
+		}
+		else{
+			if(isStar(substring)){
+				//Altera a matriz para o fecho de Kleene
+			}
+			else{
+				//Altera a matriz para o caso de concatenação
+			}
+		}
+		
+	}
+	
+	private boolean isUnion(String substring){
+		int counter = 0;
+		char aux;
+		
+		for(int i = 0; i < substring.length(); i++){
+			aux = substring.charAt(i);
+			if(aux == '(')
+				counter++;
+			if(aux == ')')
+				counter--;
+			if(aux == '+' && counter == 0)
+				return true;
+				
+		}
+		return false;
+	}
+	
+	private boolean isCat(String substring){
+		return true;
+	}
+	
+	private boolean isStar(String substring){
+		if(substring.length() > 2){
+			if(substring.charAt(substring.length()-1) == '*')
+				return true;
+		}
+			
+		return false;
 	}
 	
 }
